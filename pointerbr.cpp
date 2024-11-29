9dce2737c7308e0fb30f638f23ea2f1d721512a0
Haseeb-Syed220 <u2024220@giki.edu.pk>
2024-11-29 18:29:44 +0500

    I have commented a cluster of code and uncommented the other cluster
    just to check how git hub works

--- pointerbr.cpp
+++ pointerbr.cpp
@@ -37,65 +37,65 @@
 // //   }
 // // }
 
-// #include<iostream>
-// #include<iomanip>
-
-// using namespace std;
-
-// void bubbleSortFunction(int *lolz,int i,int n);
-// void printPointer(int *rubaab,int n);
-
-// main()
-// {
-//     int arr[]={3,33,44,55,23,53,76,45,88};
-//     int i=0;
-//     int n=sizeof(arr)/sizeof(arr[0]);
-//     cout<<"The unsorted array is: "<<endl;
-//     for(int k=0;k<n;k++)
-//     {
-//         cout<<setw(5)<<arr[k];
-//     }
-//     cout<<endl;
-//     bubbleSortFunction(arr,i,n);
-//     printPointer(arr,n);
-//     return 0;
-// }
-
-// void bubbleSortFunction(int *lolz,int i,int n)
-// {
-//     if(i<=n)
-//     {
-//         if(n-i==0)
-//         {
-//             return;
-//         }
-//         for(int j=0;j<n-i-1;j++)
-//         {
-//             if(*(lolz+j)>*(lolz+j+1))
-//             {
-//                 swap(*(lolz+j),*(lolz+j+1));
-//             }
-//         }
-//         bubbleSortFunction(lolz,i+1,n);
-//     }
-// }
-
-// void printPointer(int *rubaab,int n)
-// {
-//     cout<<"The sorted array is: "<<endl;
-//     for(int j=0;j<n;j++)
-//     {
-//         cout<<setw(5)<<*(rubaab+j);
-//     }
-// }
-
 #include<iostream>
 #include<iomanip>
 
 using namespace std;
 
+void bubbleSortFunction(int *lolz,int i,int n);
+void printPointer(int *rubaab,int n);
+
 main()
 {
-    int *p=new int();
-    cout<<p;
+    int arr[]={3,33,44,55,23,53,76,45,88};
+    int i=0;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"The unsorted array is: "<<endl;
+    for(int k=0;k<n;k++)
+    {
+        cout<<setw(5)<<arr[k];
+    }
+    cout<<endl;
+    bubbleSortFunction(arr,i,n);
+    printPointer(arr,n);
+    return 0;
 }
+
+void bubbleSortFunction(int *lolz,int i,int n)
+{
+    if(i<=n)
+    {
+        if(n-i==0)
+        {
+            return;
+        }
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(*(lolz+j)>*(lolz+j+1))
+            {
+                swap(*(lolz+j),*(lolz+j+1));
+            }
+        }
+        bubbleSortFunction(lolz,i+1,n);
+    }
+}
+
+void printPointer(int *rubaab,int n)
+{
+    cout<<"The sorted array is: "<<endl;
+    for(int j=0;j<n;j++)
+    {
+        cout<<setw(5)<<*(rubaab+j);
+    }
+}
+
+// #include<iostream>
+// #include<iomanip>
+
+// using namespace std;
+
+// main()
+// {
+//     int *p=new int();
+//     cout<<p;
+// }
